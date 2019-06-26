57dfebc0fb1556149a4273211fdb3e0a68ec10af
trungtv4597 <trungtv4597@gmail.com>
2019-06-26 19:14:24 +0700

    frist

---
+++ measure_distance.py
@@ -0,0 +1,53 @@
+# we can calculate Distance of object: D' = (F * W)/P
+
+import numpy as np
+import cv2
+import stop_sign_detection
+#from focal_length import calculate_focal_length
+
+def calculate_focal_length():
+    p = 90
+    d = 40
+    w = 5.5
+    f = ((p * d) / w)
+    return [f, w]
+
+def distance(frame, stop_coor):
+    x = calculate_focal_length()
+    f = x[0]
+    w = x[1]
+    if type(stop_coor) == tuple:
+        stop_coor
+    elif type(stop_coor) == np.ndarray:
+        p = stop_coor[0, 2]
+        d = int(((w) * f / p))
+        print('distance:',d)
+        cv2.putText(frame, 'distance object: ' + str(d) + 'cm', (100, 150),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
+
+def main_video():
+    x = calculate_focal_length()
+    f = x[0]
+    w = x[1]
+    cap = cv2.VideoCapture(0)
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+
+        stop_coor = stop_sign_detection.stop(frame) 
+        #print(stop_coor)
+
+        if type(stop_coor) == tuple:
+            stop_coor
+        elif type(stop_coor) == np.ndarray:
+            p = stop_coor[0, 2]
+            d = int(((w) * f / p))
+            print('distance:',d)
+            cv2.putText(frame, 'distance object: ' + str(d) + 'cm', (100, 150),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0))
+
+        cv2.imshow('xxx', frame)
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
+    cap.release
---
+++ stop_sign_detection.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+
+
+def stop(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    stop_cascade = cv2.CascadeClassifier('Stopsign_HAAR_19Stages.xml')
+    stop_coordinates = stop_cascade.detectMultiScale(blur, 1.3, 5)
+
+    for (x, y, w, h) in stop_coordinates:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #cv2.imshow('stop sign', image)
+        cv2.putText(image, "Stop", (100, 100),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255))
+
+    return stop_coordinates
+
+def decision_stop(stop_coordinates):
+    #if len(stop_coordinates) == 0 :
+    #    print('tiếp tục di chuyển')
+    if len(stop_coordinates) != 0:
+        print('biến báo STOP, dừng lại')
+
+# cap = cv2.VideoCapture(0)
+# while 1:
+#     _, frame = cap.read()
+#     image = stop(frame)
+#     decision(image)
+#     cv2.imshow('img',frame)
+#     k = cv2.waitKey(30) & 0xff
+#     if k == 27:
+#         break
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+# C:\Users\DucTRung\Documents\OpenCV\traffic_signs_detection\stop_sign.JPG
+# img = cv2.imread(
+#     r'C:\Users\DucTRung\Documents\OpenCV\traffic_signs_detection\stop_sign.JPG')
+# stop_coordinates = stop(img)
+
+# if len(stop_coordinates) == 0:
+#     print('di chuyển tiếp')
+# else:
+#     print('biến stop, dừng lại')
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
